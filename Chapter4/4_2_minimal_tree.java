d6fefcec633160461395d2a4ad953d456120d00e
Edgar Velazquez <edgar.velazquez@sjsu.edu>
2020-10-11 15:17:13 -0700

    4_2_binary_search

---
+++ Chapter4/4_2_minimal_tree.java
@@ -0,0 +1,34 @@
+//Leetcode Link
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/submissions/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+         return helper(nums,0,nums.length-1);
+    }
+    
+    public TreeNode helper(int[] nums, int left, int right) {
+        if(left>right) return null;
+        
+        int middle = (left+right)/2;
+        TreeNode temp = new TreeNode(nums[middle]);
+        temp.left = helper(nums,left,middle-1);
+        temp.right = helper(nums, middle+1,right);
+        return temp;
+        
+    }
+}
